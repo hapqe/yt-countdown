ee043c260b2d813356218eb4d95ba4429458c527
hapke <66736985+hapqe@users.noreply.github.com>
2023-07-25 08:51:53 +0200

    Add files via upload

---
+++ yt-countdown.user.js
@@ -0,0 +1,28 @@
+// ==UserScript==
+// @name         YouTube Countdown Timer
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Adds a countdown timer to YouTube before you can use the page.
+// @author       YourName
+// @match        https://www.youtube.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const countdownDurationSeconds = 10; // Change this value to set the countdown duration in seconds
+
+    function showCountdownAlert() {
+        let timeLeft = countdownDurationSeconds;
+        const interval = setInterval(() => {
+            alert(`You need to wait ${timeLeft} seconds before using YouTube.`);
+            timeLeft--;
+            if (timeLeft < 0) {
+                clearInterval(interval);
+            }
+        }, 1000);
+    }
+
+    showCountdownAlert();
+})();
